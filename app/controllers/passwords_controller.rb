9097d7fcd41718048be9e8e1e48b36c0ce1d00a6
Brad <bradsaid@gmail.com>
2025-09-23 11:42:21 -0500

    Remove allow_unauthenticated_access (not defined)

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,6 +1,6 @@
 class PasswordsController < ApplicationController
-  allow_unauthenticated_access
-  before_action :set_user_by_token, only: %i[ edit update ]
+  #allow_unauthenticated_access
+  skip_before_action :authenticate_user!, only: %i[new create edit update]
 
   def new
   end
